8256c152f309ec67e6dde5aeebc6d30b7ee5a85a
rafarizvi <152836959+rafarizvi@users.noreply.github.com>
2024-03-17 20:42:55 -0400

    changed code in get route

--- note-taker-app/routes/notes.js
+++ note-taker-app/routes/notes.js
@@ -2,15 +2,19 @@ const notes = require('express').Router();
 const fs = require('fs');
 const uuid = require('../helpers/uuid');
 
-const notesJson = require('../db/notes.json');
 
-// GET request for notes
-notes.get('/', (req, res) => {
-  // Send a message to the client
-  res.json(notesJson);
-  // Log our request to the terminal
-  console.log(`${req.method} request received to get notes`);
-});
+
+  notes.get('/', (req, res) => {
+    fs.readFile(`./db/notes.json`, 'utf8', (err, data) => {
+      if (err) {
+        console.error(err)
+        res.status(500).json('Error reading notes');
+      } else {
+        res.json(JSON.parse(data));
+      }
+    });
+  });
+
 
 // POST request for notes
 notes.post('/', (req, res) => {
@@ -34,6 +38,7 @@ notes.post('/', (req, res) => {
     fs.readFile(`./db/notes.json`, 'utf8', (err, data) => {
       if (err) {
         console.error(err)
+        res.json('Error adding note');
       } else {
         const parsedNotes = JSON.parse(data);
 
@@ -43,56 +48,19 @@ notes.post('/', (req, res) => {
         fs.writeFile(`./db/notes.json`, JSON.stringify(parsedNotes, null, 2), (err) =>
           err
             ? console.error(err)
-            : console.log(
-              `New note has been added to JSON file`
-            )
-        );
+            : console.log(`New note has been added to JSON file`)
+            );
+            console.log('Added new note');
+            res.status(201).json('Added new note');
       }
+      
     });
 
-
-    const response = {
-      status: 'success',
-      body: newNote,
-    };
-
-    console.log(response);
-    res.status(201).json(response);
   } else {
-    res.status(500).json('Error in adding note');
+    console.log('Add both title and text');
   }
+  
 });
-
-
-
-// // DELETE route for deleting notes
-// notes.delete('/:id', (req, res) => {
-
-//   const deleteNoteId = req.params.id;
-
-//   let noteIndex = -1;
-
-//   // Iterate through the notes to find the index of the note to delete
-//   for (let i = 0; i < notesJson.length; i++) {
-//     if (deleteNoteId === notesJson[i].id) {
-//       noteIndex = i;
-//       break; // Exit the loop once the note is found
-//     }
-//   }
-
-//   // If the note with the specified ID is found, delete it
-//   if (noteIndex !== -1) {
-//     notesJson.splice(noteIndex, 1);
-//     console.log('Deleted note');
-//     res.status(200).json('Deleted note');
-//   } else {
-//     // If the note is not found, return an error
-//     console.error('Unable to delete note');
-//     res.status(404).json('Note not found');
-//   }
-// });
-
-//=========
 
 // DELETE route for deleting notes
 notes.delete('/:id', (req, res) => {
@@ -135,7 +103,8 @@ notes.delete('/:id', (req, res) => {
       );
     };
   });
-});
 
+
+});
 
 module.exports = notes;
